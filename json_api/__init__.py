7d580e448d9571c6fc5f65e72169e2ba476a9614
Jiadeng Ruan <jiadeng@16financial.com>
2019-01-02 21:04:07 +0800

    try import frameworks

--- json_api/__init__.py
+++ json_api/__init__.py
@@ -1,2 +1,11 @@
-from .magic_sanic import MagicSanic
-from .magic_django import MagicDjango
+try:
+    import sanic
+    from .magic_sanic import MagicSanic
+except ImportError:
+    pass
+
+try:
+    import django
+    from .magic_django import MagicDjango
+except ImportError:
+    pass
